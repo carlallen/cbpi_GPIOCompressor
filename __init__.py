69d6f935523a9efb89a74b1bf69ca6148e09f5c7
Carl Allen <carl@allenofmn.com>
2017-09-27 20:26:02 -0500

    Compatibility with 3.1

--- __init__.py
+++ __init__.py
@@ -1,18 +1,27 @@
-from modules import cbpi
-from modules.base_plugins.gpio_actor import *
-from modules.core.props import Property
+from modules.core.core import cbpi
+from modules.core.basetypes import Actor
+from modules.core.proptypes import Property
 from datetime import datetime, timedelta
+
+try:
+    import RPi.GPIO as GPIO
+
+    GPIO.setmode(GPIO.BCM)
+except Exception as e:
+    print e
+    pass
 
 cbpi.gpio_compressors = []
 
-class Compressor(object):
-    c_delay = Property.Number("Compressor Delay", True, 10, "minutes")
+class Compressor(Actor):
+    c_delay = Property.Number("Compressor Delay", True, 10, description="Minium time between cycles in minutes")
+    gpio = Property.Select("GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], description="GPIO to which the actor is connected")
     compressor_on = False
     compressor_wait = datetime.utcnow()
     delayed = False
 
     def init(self):
-        super(Compressor, self).init()
+        GPIO.setup(int(self.gpio), GPIO.OUT)
         cbpi.gpio_compressors += [self]
 
     def on(self, power=0):
@@ -31,16 +40,34 @@ class Compressor(object):
         self.delayed = False
         super(Compressor, self).off()
 
-@cbpi.backgroundtask(key="update_compressors", interval=5)
+@cbpi.addon.core.backgroundjob(key="update_compressors", interval=5)
 def update_compressors(api):
     for compressor in cbpi.gpio_compressors:
         if compressor.delayed and datetime.utcnow() >= compressor.compressor_wait:
             compressor.on()
 
-@cbpi.actor
-class RelayCompressor(Compressor, RelayBoard):
-    pass
+@cbpi.addon.actor.type("Relay Board Compressor")
+class RelayCompressor(Compressor):
 
-@cbpi.actor
-class GPIOCompressor(Compressor, GPIOSimple):
-    pass
+    def init(self):
+        super(Compressor, self).init()
+        GPIO.output(int(self.gpio), 1)
+
+    def on(self, power=0):
+        GPIO.output(int(self.gpio), 0)
+
+    def off(self):
+        GPIO.output(int(self.gpio), 1)
+
+@cbpi.addon.actor.type("Simple GPIO Compressor")
+class GPIOCompressor(Compressor):
+
+    def init(self):
+        super(Compressor, self).init()
+        GPIO.output(int(self.gpio), 0)
+
+    def on(self, power=0):
+        GPIO.output(int(self.gpio), 1)
+
+    def off(self):
+        GPIO.output(int(self.gpio), 0)
